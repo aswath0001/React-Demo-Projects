364cba5e445226902c45beb3a914d2c8d99fd732
aswath0001 <rsaswaths38@gmail.com>
2025-03-03 16:41:00 +0530

    ToDo list

--- ToDo List/src/Components/ToDoList.jsx
+++ ToDo List/src/Components/ToDoList.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from "react";
+import '../Style.css';
 
 const ToDoList = () => {
+    const[todo, setToDo] = useState([]);
+    const [input, setInput] = useState('');
+    const handleSubmit = () => {
+        setToDo((todo) => {
+           return todo.concat({
+                text:input,id:Math.floor(Math.random() * 10),
+            });
+        });
+        setInput('');
+    };
+    const removetodo = id => setToDo(todo => todo.filter(t => t.id !=id));
   return (
-    <div>ToDoList</div>
+    <div className="container">
+      <input type="text" placeholder="New ToDo" value ={input} onChange={(e) => setInput(e.target.value)}/>
+      <button onClick={handleSubmit}>Submit</button>
+      <ul className="todos-list">
+        {todo.map(({text,id}) => (
+            <li className="todo" key={id}>
+                <span>{text}</span>
+                <button className="close" onClick={() => removetodo(id)}>Remove</button>
+            </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default ToDoList
+export default ToDoList;
