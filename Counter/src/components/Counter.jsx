02f7a7ec8f94e917441b30ee81aa7db13b0a1f74
aswath0001 <rsaswaths38@gmail.com>
2025-03-03 16:12:43 +0530

    Counter

--- Counter/src/components/Counter.jsx
+++ Counter/src/components/Counter.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useState } from "react"
+import "../style.css";
 
 const Counter = () => {
+    const [count ,setCount] = useState(0)
+
+    const increment =() => setCount(count+1);
+    const decrement =() => setCount(count-1);
   return (
-    <div>Counter</div>
+    <div className="container">
+        <div>
+            <h1 className="number">{count}</h1>
+        </div>
+        <div className="btns-container">
+          <button onClick={increment} className="increment">Increment</button>
+          <button onClick={decrement} className="increment">Decrement</button>
+        </div>
+    </div>
   )
 }
 
