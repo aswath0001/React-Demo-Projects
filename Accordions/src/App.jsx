220f13fd45afed261ff08b30f1525bb730eccc6a
aswath0001 <rsaswaths38@gmail.com>
2025-03-05 21:26:56 +0530

    Accordions

---
+++ Accordions/src/App.jsx
@@ -0,0 +1,16 @@
+import Accordions from "./components/Accordions";
+import { accordionData } from "./utils/content";
+
+const App = () => {
+  return (
+    <div>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordions title={title} content={content} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
